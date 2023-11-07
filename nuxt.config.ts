ea3b83e6d1bd6eb3204be448271bd8951ced2b82
demhat <mail.demhatsahin@gmail.com>
2023-11-07 17:07:18 +0100

    feat(seo): update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,8 +72,10 @@ export default defineNuxtConfig({
       viewport: 'width=device-width,initial-scale=1',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
